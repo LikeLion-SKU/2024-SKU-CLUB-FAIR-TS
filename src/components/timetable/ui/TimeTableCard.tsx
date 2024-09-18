07aba21b24f602c65d0f6731cb3418d9698c5cbb
changhoyu <tyu9817@naver.com>
2024-09-18 22:40:12 +0900

    timetable 추가

---
+++ src/components/timetable/ui/TimeTableCard.tsx
@@ -0,0 +1,59 @@
+import getImageUrl from '../../../utils/getImageUrl';
+
+interface TimeTableCardProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+export default function TimeTableCard({ children }: TimeTableCardProps) {
+  return <div className="table w-full h-[600px] bg-[#FFF5F5] rounded ">{children}</div>;
+}
+
+interface TimeTableListProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+function TimeTableList({ children }: TimeTableListProps) {
+  return <ul className="px-[20px]">{children}</ul>;
+}
+
+interface TimeTableItemProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+function TimeTableItem({ children }: TimeTableItemProps) {
+  return (
+    <li className="flex justify-between content-center w-full px-[20px] py-[10px] border-b-2 border-[#50A65D] border-dotted last:border-b-0">
+      {children}
+    </li>
+  );
+}
+
+function TimeTableLogo({ imageName }: { imageName: string }) {
+  return (
+    <div className="w-[40px] h-[40px]">
+      <img src={getImageUrl(imageName)} alt="club-logo" className=" object-cover" />
+    </div>
+  );
+}
+
+function TimeTableTimeInfo({ time }: { time: string }) {
+  return (
+    <div className="flex justify-center items-center basis-[100px]">
+      <p className=" text-center text-[16px]">{time}</p>
+    </div>
+  );
+}
+
+function TimeTableContent({ content }: { content: string }) {
+  return (
+    <div className="flex justify-center items-center basis-[150px]">
+      <p className=" text-center text-[16px] ">{content}</p>
+    </div>
+  );
+}
+
+TimeTableCard.List = TimeTableList;
+TimeTableCard.Item = TimeTableItem;
+TimeTableCard.Logo = TimeTableLogo;
+TimeTableCard.TimeInfo = TimeTableTimeInfo;
+TimeTableCard.Content = TimeTableContent;
--- src/routes/_required-header/timetable/index.tsx
+++ src/routes/_required-header/timetable/index.tsx
@@ -1,5 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router';
+import TimeTable from '../../../components/timetable/TimeTable';
 
 export const Route = createFileRoute('/_required-header/timetable/')({
-  component: () => <div>timetable page</div>,
+  component: () => <TimeTablePage />,
 });
+
+function TimeTablePage() {
+  return (
+    <main>
+      <TimeTable />
+    </main>
+  );
+}
